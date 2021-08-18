67402e97518b40c34e13cc64ad9ec8c9a7c51e6d
R4MA26 <ramaismaya25@gmail.com>
2021-08-19 01:40:06 +0800

    update

--- firebase/firebase-view-nginap.js
+++ firebase/firebase-view-nginap.js
@@ -258,4 +258,66 @@ if (hariini == "Jumat") {
   function dataGagalpiket(err) {
     console.log(err);
   }
+} else if (hariini == "Kamis") {
+  pemateringinapRef.on("value", dataBerhasilnginap, dataGagalpiket);
+  //membuat variabel untuk passing data
+  var view_nginap = document.getElementById("view_nginap");
+
+  function dataBerhasilnginap(data) {
+    //membuat variabel kosong sebagai tempat menyimpan hasil loopingan data
+    var value_piket = "";
+    var count = 0;
+    var active = " active";
+    data.forEach(function (cetak) {
+      if (cetak.val().hari == "Kamis") {
+        //jika ada data yg harinya == Sabtu
+
+        if (count == 0) {
+          value_piket +=
+            '<div class="carousel-item' +
+            active +
+            ' ">' +
+            '<img class="card-img-top img-fluid" src=" ' +
+            cetak.val().url +
+            ' " alt="Card image cap"> ' +
+            '<div class="pt-4">' +
+            '<div class="carousel-caption">' +
+            '<div class="text-center">' +
+            '<h6 class="btn btn-sm btn-primary btn-rounded text-center text-white m-0">' +
+            cetak.val().nama_pemateri +
+            "</h6>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>";
+        } else {
+          value_piket +=
+            '<div class="carousel-item">' +
+            '<img class="card-img-top img-fluid" src=" ' +
+            cetak.val().url +
+            ' " alt="Card image cap"> ' +
+            '<div class="pt-4">' +
+            '<div class="carousel-caption">' +
+            '<div class="text-center">' +
+            '<h6 class="btn btn-sm btn-primary btn-rounded text-center text-white m-0">' +
+            cetak.val().nama_pemateri +
+            "</h6>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>";
+        }
+
+        count++;
+      } else {
+        console.log("Tidak ada data");
+      }
+    });
+    //passing data dari variable tabel ke view
+    view_nginap.innerHTML = value_piket;
+  }
+
+  function dataGagalpiket(err) {
+    console.log(err);
+  }
 }
